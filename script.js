0ff9ea3031d142570ec48eb6e31377a76c7d28aa
Zainab-dont-disturb <zainabadaranijo111@gmail.com>
2024-11-05 18:22:20 +0000

    done

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.getElementById("CHANGE").innerHTML="hello world"
+var name= "Zainab";
+var age= 13;
+var a=[10,8,9,2,4,7,5,6,3,1,0]
+var b=["Fuzial","Zainab","Rosa","Jake"]
+document.getElementById("ID1").innerHTML= a.sort();
+document.getElementById("ID2").innerHTML= b.sort();
+document.getElementById("ID3").innerHTML= a.sort(function(x1,x2){
+return x1-x2
+});
